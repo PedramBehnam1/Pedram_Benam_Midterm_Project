7fbc5830aeff4baf7f3590eca145f6f59c43cd8d
PedramBehnam1 <pedrambehnam4586@gmail.com>
2021-12-03 12:24:22 +0330

    add and modify files

---
+++ assignment7/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Dark_Moder from './Dark-Mode';
+import MainPage from './MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Dark_Moder/>
+      <div id="wrapper">
+       <MainPage />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
