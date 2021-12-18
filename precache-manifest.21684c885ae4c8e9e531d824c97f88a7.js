450a4a8bfcd6f05c41bb94b7933f4cd5a34a655a
PedramBehnam1 <pedrambehnam4586@gmail.com>
2021-12-18 21:27:24 +0330

    Updates

---
+++ precache-manifest.21684c885ae4c8e9e531d824c97f88a7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d2a08207d5df182a33c",
+    "url": "/Pedram_Benam_Midterm_Project/static/css/main.a917d156.chunk.css"
+  },
+  {
+    "revision": "3d2a08207d5df182a33c",
+    "url": "/Pedram_Benam_Midterm_Project/static/js/main.3d2a0820.chunk.js"
+  },
+  {
+    "revision": "aeb7b420299a970318c2",
+    "url": "/Pedram_Benam_Midterm_Project/static/js/1.aeb7b420.chunk.js"
+  },
+  {
+    "revision": "4c249a3f0ee11f50043b",
+    "url": "/Pedram_Benam_Midterm_Project/static/js/2.4c249a3f.chunk.js"
+  },
+  {
+    "revision": "75e5cb30b2ea8ad513a5",
+    "url": "/Pedram_Benam_Midterm_Project/static/js/runtime~main.75e5cb30.js"
+  },
+  {
+    "revision": "4b16b0eb2e83a9e6efcefd6d498c15fb",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/Driblle.4b16b0eb.png"
+  },
+  {
+    "revision": "3a00e74f37cbeee4ef2faeeea715acce",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-8.3a00e74f.jpg"
+  },
+  {
+    "revision": "2f8060a8ef47e3c3c0f114acb5d4a7a5",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-1.2f8060a8.jpg"
+  },
+  {
+    "revision": "4dbd20c7fea55969d4d7fb5061072344",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-4.4dbd20c7.jpg"
+  },
+  {
+    "revision": "39225fa9eefa1d44974920acc24c287f",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-2.39225fa9.jpg"
+  },
+  {
+    "revision": "aecf168255bd7d69331046734e3a0758",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-2.aecf1682.jpg"
+  },
+  {
+    "revision": "9089610030185d11411313d67f7f37ac",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-7.90896100.jpg"
+  },
+  {
+    "revision": "66a3179f005afe910dcd2ff4dba095ae",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-7.66a3179f.jpg"
+  },
+  {
+    "revision": "27875a3c3ff7bf2ba8cf0ecac7cc8271",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-6.27875a3c.jpg"
+  },
+  {
+    "revision": "f3b1f03ab988d4f30788d1ffe096992b",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-3.f3b1f03a.jpg"
+  },
+  {
+    "revision": "33bd1c372889d095deddef5598ff8ca8",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-3.33bd1c37.jpg"
+  },
+  {
+    "revision": "b2e2740a7c85955e84d29c5bd95221fe",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-8.b2e2740a.jpg"
+  },
+  {
+    "revision": "410e3af7c2722c67e767cf885cf6a476",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-5.410e3af7.jpg"
+  },
+  {
+    "revision": "3c7758d94453e9e13cc900dac0ff8083",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/mockup-6.3c7758d9.jpg"
+  },
+  {
+    "revision": "78122b483092a7211812c14af05c032f",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo20.78122b48.PNG"
+  },
+  {
+    "revision": "fabebc6a8e619e5e407c72d248a4bbf3",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-21.fabebc6a.PNG"
+  },
+  {
+    "revision": "0d6911ad48a6a33499bf23291dc49b61",
+    "url": "/Pedram_Benam_Midterm_Project/static/media/logo-22.0d6911ad.PNG"
+  },
+  {
+    "revision": "d3065ca851fa6359b2398958d0451dbe",
+    "url": "/Pedram_Benam_Midterm_Project/index.html"
+  }
+];
